5dfac08c682aa1c8e21f7a4acd379133107b8fd2
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-04-28 20:13:43 +0800

    Update prototype next trains display code

--- nexttrains/alpha.lua
+++ nexttrains/alpha.lua
@@ -1,14 +1,29 @@
-function printRecord(trip,time,dest)
-    return string.format("%-10s%4ss\n%-15s\n", trip, time, dest)
+function printRecord(trip,time,dest,invalid)
+    local destChopped
+    if string.len(dest) <= 16 or invalid then
+        destChopped = dest
+    else
+        local destChopTick = ticks % (string.len(dest) - 15 + 4) + 1
+        destChopped = string.sub("  " .. dest, destChopTick)
+    end
+    local time = invalid and "-" or time
+
+    return string.format("%3.3s %-5.5s %-.16s\n", time, trip, destChopped)
 end
 
-if event.on and event.pin.name=="B" then
-    digiline_send("lcd_2",
-        string.format("\n\n\n\n  Time:  %s",
-                      rwt.to_string(rwt.now(), true)))
-    digiline_send("lcd_1",
-                  ": Trains test :\n" ..
-                  printRecord("L107", 12,"Ikceok") ..
-                  printRecord("R103", 63,"Parisem") .. 
-                  printRecord("L888",888,"Langcottonspringwoodcester"))
+if event.int and event.msg.type=="update" and event.msg.count<=7 then
+    ticks = (ticks or -1) + 1
+    local i = event.msg.count > 6
+    digiline_send("allnext_long",
+                  string.format("From this station %8.8s\n",
+                   i and "" or rwt.to_string(rwt.now(), true)) ..
+                  printRecord("L107","12s","Ikceok",i) ..
+                  printRecord("R103","58s","Omenutleikque",i) .. 
+                  printRecord("L888a","8m","Langcottonspringwoodcester",i))
+    if i then ticks = -1 end
+    clear_interrupts()
+    interrupt(0.5, {type="update", count=event.msg.count + 1})
+elseif (event.on or event.off) and event.pin.name=="D" then
+    clear_interrupts()
+    interrupt(0, {type="update", count=1})
 end
